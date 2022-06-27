bd718e640b3fc568b427c4582e8374d3b0b1b1a2
E5P10 <lucca360@outlook.com>
2022-06-27 09:01:31 -0500

    update: Nav_bar como componente

---
+++ views/components/nav_bar.php
@@ -0,0 +1,40 @@
+<nav class="nav-bar">
+    <h1><a href=""><span class="logo-title logo-icon"></span></a></h1>
+    <div>
+        <form class="nav-bar__barra_busqueda" action="#" method="get"> 
+            <select name="curso" id="curso">
+                <option class="hidden_option" selected disabled>Curso</option>
+                <optgroup label="1ro">
+                    <option value="curso_1">Curso 1</option>
+                    <option value="curso_2">Curso 2</option>
+                </optgroup>
+                <optgroup label="2do">
+                    <option value="curso_3">Curso 3</option>
+                    <option value="curso_4">Curso 4</option>
+                </optgroup>
+                <optgroup label="3ro">
+                    <option value="curso_5">Curso 5</option>
+                    <option value="curso_6">Curso 6</option>
+                </optgroup>
+                <optgroup label="4to">
+                    <option value="curso_7">Curso 7</option>
+                    <option value="curso_8">Curso 8</option>
+                </optgroup>
+                <optgroup label="5to">
+                    <option value="curso_9">Curso 9</option>
+                    <option value="curso_10">Curso 10</option>
+                </optgroup>
+            </select>
+            <input name="tema" id="tema" placeholder="Buscar tema">
+            <button type="submit"><span class="span-icon search-icon"></span></button>
+        </form>
+    </div>
+    <a href="#" class="nav-bar__publicar-pregunta">+ Publicar Pregunta</a>
+    <div class="nav-bar__perfil">
+        <a href="#" class="nav-bar__administrador"><span class="admin-icon"></span></a>
+        <a href="#" class="nav-bar__perfil__user-name">
+            <span class="user-image"></span> 
+            <p>Usuario 1</p>
+        </a>
+    </div>
+</nav>
