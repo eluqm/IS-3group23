6cf74ffaa769f9f7e64dc43a529a1ab0577cd222
E5P10 <lucca360@outlook.com>
2022-06-28 11:46:06 -0500

    add: database schema

---
+++ database/db__data.sql
@@ -0,0 +1,24 @@
+INSERT INTO usuario
+VALUES (20008080, 'Administrador1', 'admin01@unsa.edu.pe'),
+		(20008081, 'Administrador2', 'admin02@unsa.edu.pe'),
+        (20008082, 'Administrador3', 'admin03@unsa.edu.pe'),
+        (20008083, 'Administrador4', 'admin04@unsa.edu.pe'),
+        (20008084, 'Administrador5', 'admin05@unsa.edu.pe'),
+        (20008085, 'Administrador6', 'admin06@unsa.edu.pe'),
+        (20008086, 'Usuario1', 'user01@unsa.edu.pe'),
+        (20008087, 'Usuario2', 'user02@unsa.edu.pe'),
+        (20008088, 'Usuario3', 'user03@unsa.edu.pe'),
+        (20008089, 'Usuario4', 'user04@unsa.edu.pe'),
+        (20008090, 'Usuario5', 'user05@unsa.edu.pe'),
+        (20008091, 'Usuario6', 'user06@unsa.edu.pe'),
+        (20008092, 'Usuario7', 'user07@unsa.edu.pe'),
+        (20008093, 'Usuario8', 'user08@unsa.edu.pe');
+        
+INSERT INTO administrador VALUES (20008080),
+								(20008081),
+                                (20008082),
+                                (20008083),
+                                (20008084),
+                                (20008085),
+                                (20008086);
+                                
