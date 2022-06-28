628388d0074308cd5c72be5e54110d4b99808428
SVilcaLim03 <66433673+SVilcaLim03@users.noreply.github.com>
2022-06-28 11:31:16 -0500

    Add files via upload

--- models/usuario.php
+++ models/usuario.php
@@ -6,10 +6,10 @@ class User {
     public function register($data){
         $sql = "INSERT INTO users (usersName, usersUid,usersEmail, usersPwd) VALUES (:usersName,:usersEmail,:usersUid,:usersPwd)";
         $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':usersName', $_POST['usersName']);
-        $stmt->bindParam(':usersUid', $_POST['usersUid']);
-        $stmt->bindParam(':usersEmail', $_POST['usersEmail']);
-        $password = password_hash($_POST['usersPwd'], PASSWORD_BCRYPT);
+        $stmt->bindParam(':usersName', $data['usersName']);
+        $stmt->bindParam(':usersUid', $data['usersUid']);
+        $stmt->bindParam(':usersEmail', $data['usersEmail']);
+        $password = password_hash($data['usersPwd'], PASSWORD_BCRYPT);
         $stmt->bindParam(':usersPwd', $password);
 
         if ($stmt->execute()) {
