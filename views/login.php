9022187c9ee212a7295998d6959f4752575a61ec
Solange4 <sromeroc@unsa.edu.pe>
2022-06-28 21:24:42 -0500

    Update login/signup

--- views/login.php
+++ views/login.php
@@ -1,10 +1,34 @@
-<h1 class="header">Please Login</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<form method="post" action="./controllers/Users.php">
-<input type="hidden" name="type" value="login">
-    <input type="text" name="name/email"  
-    placeholder="Username/Email...">
-    <input type="password" name="usersPwd" 
-    placeholder="Password...">
-    <button type="submit" name="submit">Log In</button>
-</form>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/login.css">
+
+    <title>Iniciar Sesion</title>
+</head>
+
+<body>
+    <div class="login-container">
+        <div class="image-container">
+            <img src="icons/logo.png" width="300" height="150px">
+            <img src="icons/CsLogo.png" width="170" height="150px">
+            <p>Tasti es un intermediario para reunir a estudiantes de Ciencia de la Computación: si un estudiante tiene dificultades para aprender sobre un tema específico puede postearlo en nuestro foro y si otro estudiante tiene conocimientos de ese tema
+                puede hacerle un reforzamiento por medio de una conferencia enlace meet, además de permitir a otros estudiantes con dudas similares permitir unirse a la presente explicación.</p>
+        </div>
+        <div class="login-info-container">
+            <h1 class="title">Iniciar Sesión</h1>
+            <form class="inputs-container" method="post" action="./controllers/Users.php">
+                <input class="input" name="name/email" type="text" placeholder="Ingresar correo institucional">
+                <input class="input" type="password" name="usersPwd" placeholder="Ingresar contraseña">
+                <button class="btn" type="submit" name="submit">Iniciar Sesión</button>
+                <p>¿No tienes cuenta?<a class="a" href="signup.php">Registrate</a></p>
+                <img src="icons/login.svg" width="300" height="400px">
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
--- views/signup.php
+++ views/signup.php
@@ -1,14 +1,45 @@
-<h1 class="header">Please Signup</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<form method="post" action="./controllers/Users.php">
-    <input type="hidden" name="type" value="register">
-    <input type="text" name="usersName" 
-    placeholder="Full name...">
-    <input type="text" name="usersEmail" 
-    placeholder="Email...">
-    <input type="text" name="usersUid" 
-    placeholder="Username...">
-    <input type="password" name="usersPwd" 
-    placeholder="Password...">
-    <button type="submit" name="submit">Sign Up</button>
-</form>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/signup.css">
+    <title>Registrarse</title>
+</head>
+
+<body>
+    <div class="login-container">
+        <div class="image-container">
+            <h1 class="title">Registrarse</h1>
+            <img src="icons/registro.svg" width="450" height="500px">
+        </div>
+        <div class="login-info-container">
+            <form class="inputs-container" method="post" action="./controllers/Users.php">
+                <label for="fname" >Nombre:</label>
+                <input class="input" name="usersName" type="text" placeholder="Ingresar nombre completo" required>
+                <label for="fname">Correo Institucional:</label>
+                <input class="input" name="usersEmail" type="email" placeholder="Ingresar correo institucional" required> 
+                <label for="fname">CUI:</label>
+                <input class="input" name="usersCUI" type="text" placeholder="Ingresar CUI" required>
+                <label for="fname">Año:</label>
+                <select class="select" name="usersAño">
+                        <option value="primero">1ro</option>
+                        <option value="segundo">2do</option>
+                        <option value="tercero">3ro</option>
+                        <option value="cuarto">4to</option>
+                        <option value="quinto">5to</option>
+                </select>
+                <label for="fname">Contraseña:</label>
+                <input class="input" name="usersPwd" type="password" placeholder="Ingresar contraseña" required>
+                <label for="fname">Repetir contraseña:</label>
+                <input class="input" name="usersPwd-repeat" type="password" placeholder="Ingresar contraseña" required>
+                <button class="btn">Registrarse</button>
+                <p>¿Ya tienes cuenta?<a class="a" href="login.php">Iniciar Sesión</a></p>
+            </form>
+        </div>
+    </div>
+
+</body>
+
+</html>
