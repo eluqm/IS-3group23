ec43d04ea3b19f1c6b8269e92f7b707f2c0b7ddc
JimHuertas <live.land18@gmail.com>
2022-07-06 03:32:18 -0500

    adding pregunta view n css

---
+++ views/pregunta.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Mi Perfil</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/general_style.css">
+        <link rel="stylesheet" href="components/nav_bar.css">
+        <link rel="stylesheet" href="css/pregunta.css">
+        <link rel="stylesheet" href="css/main_pregunta.css">
+        <link rel="stylesheet" href="components/pregunta.css">
+    </head>
+    <body>
+        <header>
+            <?php
+            include 'components/nav_bar.php';
+            ?>
+        </header>
+
+
+        
+    <main class="main-usando-navbar ">
+        <section class="main__contenido"> 
+            <div class="main__contenido__q-list">
+            <div class="main_pregunta">
+            <div class="pregunta__contenido">
+                <p class="fecha">22/22/22</p>
+                <h2>Titulo de la pregunta?</h2>
+                <br/><hr><br/>
+                <p class="parrafo">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.</p>
+            </div>
+            </div>
+            </div>
+        </section>
+
+        <aside>
+            <br/>
+            <div class="pregunta__contenido_info">
+                <p>ID: 1234123412</p>
+                <a href="#"><span class="main_pregunta-icon edit-icon"></span></a>
+                <a href="#"><span class="main_pregunta-icon delete-icon"></span></a>
+            </div>
+
+            <div class="main_pregunta">
+            <div class="pregunta__contenido">
+                <p class="info">Curso:</p>
+                <p class="info">Estructuras de Datos</p>
+                <br/>
+                <p class="info">Tema:</p>
+                <p class="info">Estructuras de Datos</p>
+                <br/>
+                <p class="info">Usuario:</p>
+                <p class="info">Estructuras de Datos</p>
+                <br/>
+                <p class="info">Disponibilidad:</p>
+                <p class="info">Estructuras de Datos danwfwanifwa fnawi fniawnf iwanf iwan fiawn fiw nfiaw fniwa nifni wf asd ad asd ansd nasu dnuasnd uas nduasnduans udansu dnasugtfhtf hf t hft h ft h ft hft h ft  hthtfhfthtf hfth fthft hft hft hft h</p>
+                <br/>
+            </div>
+            </div>
+            <button class="aside__button-log-out">
+                ENSE&Ntilde;AR
+            </button>
+        </aside>
+
+    </main>
+
+
+        <script src="" async defer></script>
+    </body>
+
+</html>
