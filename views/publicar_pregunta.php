eed56e79ae9138c08cd09b346f1289bcddae906e
SVilcaLim03 <66433673+SVilcaLim03@users.noreply.github.com>
2022-07-17 20:03:44 -0500

    Improved_questions_management

--- views/publicar_pregunta.php
+++ views/publicar_pregunta.php
@@ -1,3 +1,7 @@
+<?php 
+    session_start(); 
+?>
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -28,46 +32,50 @@
             <div class="publicar_pregunta">
             <h1>Publicar Pregunta</h1>
             <br/>
-            <form action="">
+            <form class="inputs-container" method="post" action="../controllers/pregunta.php">
+                <input type="hidden" name="type" value="store">
                 <div id="lateral">
                     <p>
                     <label for="">T&iacute;tulo</label>
-                    <input type="text">
+                    <input type="text" name="titulo">
                     </p><br/>
 
                     <p>
                     <label for="">Tema</label>
-                    <select name="transporte">
-                    <option>Coche</option>
-                    <option>Avión</option>
-                    <option>Tren</option>
-                    </select>
-                    </p><br/>
-
-                    <p>
-                    <label for="">Horario Disponible</label>
-                    <input type="time" value="12:00:00" max="22:30:00" min="7:00:00" step="1">
+                    <input type="text" name="tema">
                     </p><br/>
 
                     <p>
                     <label for="">Fecha L&iacute;mite</label>
-                    <input type="date">
+                    <input type="datetime-local" value="2000-01-01T00:00:00" name="fecha_limite">
                     </p><br/>
                 </div>
                     
                 <div id="principal">
+                    
+
                     <p>
                     <label for="">Curso</label>
-                    <input type="text">
+                        <select name="curso">
+                            <?php
+                                include '../models/curso.php';
+                                $curso_display=new Curso;
+                                $data_cursos=$curso_display->get_all();
+                                while ($i<20) {
+                                    $i++;
+                                    echo '<option value="'.$data_cursos[$i]->nombre.'">'.$data_cursos[$i]->nombre.'</option>';
+                                }
+                            ?>
+                        </select>
+                    </form>
                     </p><br/>
 
                     <p class="input-file-wrapper">
                     <label class="descp" for="">Descripcion</label>
-                    <textarea name="" id="" cols="30" rows="7"></textarea>
+                    <textarea name="descripcion" id="" cols="30" rows="7"></textarea>
                     </p><br/>
-
                     <p class="boton">
-                    <button>PREGUNTAR</button>
+                    <button type="submit" name="submit">Preguntar</button>
                     </p>
                 </div>
             </form>
