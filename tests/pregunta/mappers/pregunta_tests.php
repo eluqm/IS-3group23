64e05a2da71f00a11bcf53a136d26556416e4b68
Santiago VL <svilcalim@unsa.edu.pe>
2022-08-13 15:08:20 -0500

    PHPUnitTests

---
+++ tests/pregunta/mappers/pregunta_tests.php
@@ -0,0 +1,36 @@
+<?php
+namespace IS3group3\Tests\pregunta\mappers\PreguntaModelsTest;
+use IS3group3\Tests\pregunta\DatabaseTestCase;
+
+class PreguntaModelsTest extends DatabaseTestCase
+{
+    public function getDataSet()
+    {
+        return $this->createArrayDataSet(array());
+    }
+    public function test_register()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+    public function test_register_in_non_rejected()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+    public function test_get_all_by_curso()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+    public function test_findQuestionById()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+    public function test_edit()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+    public function test_delete()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+}
+?>
