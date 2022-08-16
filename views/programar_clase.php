929da24a6e4369055d677b336f1778f58b5d4991
E5P10 <lucca360@outlook.com>
2022-08-16 18:32:32 -0500

    style: pregunta > programar clase

--- views/programar_clase.php
+++ views/programar_clase.php
@@ -7,7 +7,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Mi Perfil</title>
+        <title>Programar Clase</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
